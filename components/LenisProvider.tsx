05129b7eaa9da7ceb9df20f9c91e8faff58d5ddc
admin-Vulf <admin@vulf.app>
2026-05-18 01:38:37 -0500

    feat: add LenisProvider for smooth scroll with GSAP ScrollTrigger sync

---
+++ components/LenisProvider.tsx
@@ -0,0 +1,32 @@
+// components/LenisProvider.tsx
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+import { gsap, ScrollTrigger } from "@/lib/gsap";
+
+export default function LenisProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const raf = (time: number) => lenis.raf(time * 1000);
+    gsap.ticker.add(raf);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(raf);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
